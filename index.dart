ca10b1ac32a9c7d8a4118470e1c184b4920b84f9
Afzal Khan <165161281+afzalkhan90@users.noreply.github.com>
2024-04-22 03:54:07 -0700

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+void main(){
+    // for(int i = 10; i > 0; i--)
+    // {
+    //        print("$i : Afzal Khan");
+    // }
+    // for(int i = 1; i < 11; i++)
+    // {
+    //        print("$i : Afzal Khan");
+    // }
+// int i = 0; 
+// while(i < 10)
+// {
+//   if(i == 8)
+//   {
+//     break;
+//   }
+//   i++;
+//   print(i);
+// }
+
+// int i = 0; 
+// while(i < 10)
+// {
+//   i++;
+//   if(i == 8)
+//   {
+//     continue;
+//   }
+  
+//   print(i);
+// }
+// int i = 10;
+// do{
+//   print("$i Afzal");
+//   i++;
+// }while(i <= 25);
+
+// print("Enter Your Name...");
+// String? name  = stdin.readLineSync();
+
+// switch(name)
+// {
+//      case"Murtuza":
+//      print("Are You Welcome -- Murtuza");
+//      break;
+//      case"Afzal":
+//      print("Are You Welcome -- Afzal");
+//      break;
+//      case"Suffiyan":
+//      print("Are You Welcome -- Suffiyan");
+//      break;
+//      default:
+//      print("Not Find out");
+// }
+
+// print("Enter Your Age");
+// int? age = int.parse(stdin.readLineSync()!);
+
+// if(age >= 45)
+// {
+//   print("Are You Old");
+// }else if(age >= 15)
+// {
+//   print("Are You Young");
+// }else if(age >= 1)
+// {
+//   print("Are You Child");
+// }
+
+// print("Enter Number");
+// int? ev = int.parse(stdin.readLineSync()!);
+
+
+// if(ev % 2 == 0)
+// {
+//   print("$ev -- Even Number");
+// }else
+// {
+//   print("$ev -- odd Number");
+// }
+
+// print("Please! Enter Table Number");
+// int? table = int.parse(stdin.readLineSync()!);
+
+// for(int i = 1; i <= 10; i++)
+// {
+//   print("$table x $i = ${table * i }");
+// }
+
+// print("Please Enter Email");
+// String? email = stdin.readLineSync();
+
+// print("Enter Password");
+// String? pass = stdin.readLineSync();
+
+// if(email == "afzal@gmail.com" && pass == "afzal1234")
+// {
+//   print("Login Successfully");
+// }else{
+//   print("Email & Password Does not Match");
+// }
+}
